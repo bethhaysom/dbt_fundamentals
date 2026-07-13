eb824f513c2247b32bbf67e771491eeda2fcd526
Bethany Haysom <bethany.haysom@gmail.com>
2026-07-13 16:28:56 +0000

    added data tests

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -9,14 +9,14 @@ source as (
 renamed as (
 
     select
-        id,
-        user_id,
+        id as order_id,
+        user_id as customer_id,
         order_date,
-        status,
+        status as order_status,
         _etl_loaded_at
 
     from source
 
 )
 
-select * from renamed
+select distinct order_status from renamed
--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -1,8 +1,24 @@
-select 
-    id as payment_id
-    , orderid as order_id
-    , paymentmethod
-    , status as payment_status
-    , amount as payment_amount
-    , created as payment_date
-from {{ source('stripe', 'payment') }}
+with 
+
+source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod,
+        status as payment_status,
+        amount / 100 as payment_amount,
+        created,
+        _batched_at
+
+    from source
+
+)
+
+select * from renamed
